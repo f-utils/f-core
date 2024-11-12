2260bacd8486bb1b3622f7bf19424727b1127640
yxm <yxm.dev@gmx.ie>
2024-11-12 17:59:02 -0300

    ...

--- futils/mods/json.py
+++ futils/mods/json.py
@@ -159,9 +159,9 @@ def grep(json_data, search_string, case_sensitive=True):
                 current_segment = stack.p
                 iter.add(expression, current_segment)
             elif token.upper() in ("AND", "OR", "NOT"):
-                iter.add(comp.up(token), current_segment)
-            elif op.eq(comp.up(token), "WHERE"):
-                if (i + 1) < len(tokens) and comp.up(tokens[i + 1]) not in ("AND", "OR", "NOT", "(", ")"):
+                iter.add(token.upper(), current_segment)
+            elif op.eq(token.upper(), "WHERE"):
+                if (i + 1) < len(tokens) and tokens[i + 1].upper() not in ("AND", "OR", "NOT", "(", ")"):
                     prev_token = current_segment.p
                     where_term = tokens[i + 1]
                     iter.add((prev_token, where_term), current_segment)
@@ -170,7 +170,6 @@ def grep(json_data, search_string, case_sensitive=True):
                 iter.add(token, current_segment)
             i += 1
         return current_segment
-
     def process_expression(json_content, expression_segment):
         if op.bl(expression_segment, tuple):
             subexpr, subwhere = expression_segment
