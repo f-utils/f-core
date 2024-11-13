fe8ec2dc50aaa90d00c3bd178147ce77675d5a05
yxm <yxm.dev@gmx.ie>
2024-11-13 11:41:56 -0300

    .

--- futils/__init__.py
+++ futils/__init__.py
@@ -4,3 +4,16 @@ from futils.mods import path as P
 from futils.mods import json as J
 from futils.mods import http as H
 from futils.mods import sub as S
+
+
+# error classes
+class ReErr(Exception):
+    pass
+class PathErr(Exception):
+    pass
+class JsonErr(Exception):
+    pass
+class HttpErr(Exception):
+    pass
+class SubErr(Exception):
+    pass
