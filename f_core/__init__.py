b30cac4f39cd68a27ba9526c152672352290c3e6
yxm <yxm.dev@gmx.ie>
2025-01-28 12:05:18 -0300

    feat: added the basic type operations in __init__.py

--- f_core/__init__.py
+++ f_core/__init__.py
@@ -1,5 +1,18 @@
-from f_core.mods.type_ import *
+from f_core.mods.type_ import TypedFunc
+from f_core.mods.type_ import Specs
 from f_core.mods.func_ import *
-from f_core.mods.mon_ import *
+from f_core.mods.mon_  import *
 from f_core.mods.spec_ import *
-from f_core.mods.var_ import *
+from f_core.mods.var_  import *
+
+# set primitive types
+class Types:
+    tfunc = TypedFunc
+    tf = tfunc
+t = Types
+
+# set primitive specs
+prod_   = f.ds('prod_')
+coprod_ = f.ds('coprod_')
+unprod_ = f.ds('unprod_')
+func_   = f.ds('func_')
