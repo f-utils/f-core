a4a728fbe00cb6a7c1419a61973ae076a842f1da
yxm <yxm.dev@gmx.ie>
2025-01-28 18:51:20 -0300

    feat: added flexible case to prod_, func_, tfunc_, etc

--- f_core/mods/type_.py
+++ f_core/mods/type_.py
@@ -1,27 +1,28 @@
-from f import f
 import inspect
-from typing import get_type_hints
-
+from f import f
+from f_core.mods.utils import flat_, func_instance_
 
 class TypedFunc:
     def __init__(self, func):
         if not callable(func):
             raise TypeError(f"'{func}' is not a function.")
         self.func = func
-        self.signature = inspect.signature(func)
         self._domain = self.calculate_domain()
         self._codomain = self.calculate_codomain()
 
     def calculate_domain(self):
-        param_types = [
-            param.annotation if param.annotation != inspect.Parameter.empty else type(None)
-            for param in self.signature.parameters.values()
-        ]
-        return Builder.prod_type_(*param_types)
+        type_hints = get_type_hints(self.func)
+        domain_hints = tuple(type_hints.values())[:-1]
+        if domain_hints:
+            return Builder.prod_type_(*domain_hints)
+        return type(None)
 
     def calculate_codomain(self):
-        return_annotation = inspect.signature(self.func).return_annotation
-        return return_annotation if return_annotation != inspect.Signature.empty else type(None)
+        type_hints = get_type_hints(self.func)
+        return_hint = list(type_hints.values())[-1]
+        if return_hint:
+            return return_hint
+        return type(None)
 
     @property
     def domain(self):
@@ -48,43 +49,58 @@ class TypedFunc:
 
 class Builder:
     def coprod_type_(*types):
-        if len(types) == 0:
-            return type(None)
-        elif len(types) == 1:
-            return types[0]
+        flat_types = flat_(*types)[0]
+        is_flexible = flat_(*types)[1]
 
-        for typ in types:
-            if not isinstance(typ, type):
-                raise TypeError(f"{typ} is not a valid type.")
+        if len(flat_types) == 0:
+            return type(None)
+        elif len(flat_types) == 1 and not is_flexible:
+           return flat_types[0]
 
         class _coprod(type):
             def __instancecheck__(cls, instance):
-                return any(isinstance(instance, typ) for typ in cls._types)
+                if type(instance) not in flat_types:
+                    return False
+                return True
 
-        class coprod_(metaclass=_coprod):
-            _types = types
+        if is_flexible:
+            class_name = f"coprod_[{', '.join(t.__name__ for t in flat_types)}]"
+        else:
+            class_name = f"coprod_({', '.join(t.__name__ for t in flat_types)})"
+        coprod_ = _coprod(class_name, (), {})
+
         return coprod_
 
     def prod_type_(*types):
-        if len(types) == 0:
-            return type(None)
-        elif len(types) == 1:
-            return types[0]
+        flat_types = flat_(*types)[0]
+        is_flexible = flat_(*types)[1]
 
-        for typ in types:
-            if not isinstance(typ, type):
-                raise TypeError(f"{typ} is not a valid type.")
+        if len(flat_types) == 0:
+            return type(None)
+        elif len(flat_types) == 1 and not is_flexible:
+            return flat_types[0]
 
         class _prod(type):
             def __instancecheck__(cls, instance):
                 if not isinstance(instance, tuple):
                     return False
-                if len(instance) != len(cls._types):
-                    return False
-                return all(isinstance(elem, typ) for elem, typ in zip(instance, cls._types))
+                if not is_flexible:
+                    if len(instance) != len(flat_types):
+                        return False
 
-        class_name = f"prod_({', '.join(t.__name__ for t in types)})"
-        prod_ = _prod(class_name, (), {"_types": types})
+                if is_flexible:
+                    for typ in (type(x) for x in instance):
+                        if not typ in flat_types:
+                            return False
+                    return True
+                return all(isinstance(elem, typ) for elem, typ in zip(instance, flat_types))
+
+        if is_flexible:
+            class_name = f"prod_[{', '.join(t.__name__ for t in flat_types)}]"
+        else:
+            class_name = f"prod_({', '.join(t.__name__ for t in flat_types)})"
+        prod_ = _prod(class_name, (), {})
+
         return prod_
 
     def unprod_type_(*types):
@@ -116,50 +132,93 @@ class Builder:
         return unprod_
 
     def func_type_(*domain_types):
+        if not domain_types:
+            raise TypeError("Domain types cannot be empty.")
+        for typ in domain_types:
+            if not isinstance(typ, type) and not isinstance(type, list):
+                raise TypeError(f"{typ} is not a valid type.")
+        is_flexible = False
+        flat_types = []
+
+        if len(domain_types) == 1 and isinstance(domain_types[0], list):
+            is_flexible = True
+            flat_types = tuple(domain_types[0])
+        else:
+            flat_types = domain_types
+
         class _func(type):
             def __instancecheck__(cls, instance):
+                if isinstance(instance, TypedFunc):
+                    instance = instance.func
                 if not callable(instance):
                     return False
 
                 type_hints = get_type_hints(instance)
-                param_hints = list(type_hints.values())[:-1]
+                param_hints = tuple(type_hints.values())[:-1]
 
-                return param_hints == list(domain_types)
+                if is_flexible:
+                    for x in param_hints:
+                        if not x in flat_types:
+                            return False
+                    return True
+                return param_hints == flat_types
 
         class func_(metaclass=_func):
-            _types = domain_types
+            _types = flat_types
+
         return func_
 
-    def typed_func_type_(*domain_types, cod):
+    def typed_func_type_(*domain_types, cod=None):
         if not domain_types:
             raise TypeError("Domain types cannot be empty.")
         for typ in domain_types:
-            if not isinstance(typ, type):
+            if not isinstance(typ, type) and not isinstance(typ, list):
                 raise TypeError(f"{typ} is not a valid type.")
-        if not isinstance(cod, type):
-            raise TypeError(f"{cod} is not a valid type.")
+        if cod:
+            if not isinstance(cod, type):
+                raise TypeError(f"{cod} must be a type type.")
+        else:
+            raise AttributeError("Argument 'cod' must be provided.")
+
+        if len(domain_types) == 1 and isinstance(domain_types[0], list):
+            flat_types = tuple(domain_types[0])
+            is_flexible = True
+        else:
+            flat_types = domain_types
+            is_flexible = False
 
         class _typed_func(type):
             def __instancecheck__(cls, instance):
                 if not isinstance(instance, TypedFunc):
                     try:
                         instance = TypedFunc(instance)
-                        return instance
                     except:
-                       return False
+                        return False
+                if not callable(instance):
+                    return False
 
-                is_domain_match = isinstance(Builder.prod_type_(*domain_types), type(instance.domain))
-                is_codomain_match = isinstance(cod, type(instance.codomain))
+                type_hints = get_type_hints(instance.func)
+                domain_hints = tuple(type_hints.values())[:-1]
+                return_hint = tuple(type_hints.values())[-1]
+                if not return_hint == cod:
+                    return False
 
-                return is_domain_match and is_codomain_match
+                if is_flexible:
+                    for x in domain_hints:
+                        if not x in flat_types:
+                            return False
+                    return True
+                return domain_hints == flat_types
 
-        domain_type_names = ", ".join(t.__name__ for t in domain_types)
-        class_name = f"tfunc_({domain_type_names}; {cod.__name__})"
+        if is_flexible:
+            domain_type_names = "["+", ".join(t.__name__ for t in flat_types)+"]"
+        domain_type_names = ", ".join(t.__name__ for t in flat_types)
+        class_name = f"tfunc_({{{domain_type_names}}}; {cod.__name__})"
 
         return _typed_func(class_name, (), {})
 
-    def sub_type_(X, f):
-        class _sub(X):
+    def sub_type_(parent, f):
+        class _sub(parent):
             def __init__(self, *args, **kwargs):
                 super().__init__(*args, **kwargs)
                 if not f(self):
