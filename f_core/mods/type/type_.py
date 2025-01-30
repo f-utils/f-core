d77cfdce1fec2c6c3d669a3ff2bc3285e6beb898
yxm <yxm.dev@gmx.ie>
2025-01-30 14:56:27 -0300

    feat: add primitive function types

--- f_core/mods/type/type_.py
+++ f_core/mods/type/type_.py
@@ -1,171 +1,229 @@
 import inspect
 from typing import get_type_hints
 from f import f
-from f_core.mods.type.op_ import prod_type_
-from f_core.mods.type.helper_ import flat_, func_instance_
+from f_core.mods.type.helper_ import (
+        runtime_domain,
+        runtime_codomain,
+        runtime_comp,
+        hinted_domain,
+        hinted_codomain,
+        hinted_comp,
+        check_domain,
+        check_codomain,
+        typed_comp
+)
 
-# TODO
-class Func:
+# -----------------------------
+#       Plain Functions
+# -----------------------------
+class PlainFunc:
     """
-    The class of 'functions':
+    The class of 'plain functions':
         1. objects are callable
-        2. defined composition
+        2. defined comp
     """
-    pass
+    def __init__(self, func):
+        if not callable(func):
+            raise TypeError(f"'{func}' is not callable.")
+        self.func = func
 
-# TODO
-class DomRuntimedFunc:
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __mul__(self, other):
+        if not isinstance(other, PlainFunc):
+            raise TypeError(f"'{other}' is not a valid plain function.")
+        def comp(*args, **kwargs):
+            return self.func(other(*args, **kwargs))
+        return PlainFunc(comp)
+
+
+# -----------------------------
+#       Runtimed Functions
+# -----------------------------
+class RuntimedDomFunc(PlainFunc):
     """
     The class of 'domain-runtimed functions':
         1. defined domain (at runtime)
-        2 defined composition
-    It is a subclass of 'Func'
+        2 defined comp
+    It is a subclass of 'PlainFunc'
     """
-    pass
+    def __init__(self, func):
+        super().__init__(func)
+        self._runtime_domain = runtime_domain(func)
 
-# TODO
-class CodRuntimedFunc:
+    @property
+    def domain(self):
+        return self._runtime_domain
+
+class RuntimedCodFunc:
     """
     The class of 'codomain-runtimed functions':
         1. defined codomain (at runtime)
-        2. defined composition
-    It is a subclass of 'Func'
+        2. defined comp
+    It is a subclass of 'PlainFunc'
     """
-    pass
+    def __init__(self, func):
+        super().__init__(func)
+        self._runtime_codomain = runtime_codomain(func)
 
-# TODO
+    @property
+    def codomain(self):
+        return self._runtime_codomain
+
 class RuntimedFunc:
     """
     The class of 'runtimed functions':
         1. defined domain and codomain (at runtime)
-        2. safe composition (checked at runtime)
+        2. safe comp (checked at runtime)
     It is a subclass of:
         1. 'DomRuntimedFunc'
         2. 'CodRuntimedFunc'
     """
-    pass
+    def __init__(self, func):
+        RuntimedDomFunc.__init__(self, func)
+        RuntimedCodFunc.__init__(self, func)
 
-# TODO
-class DomHintedFunc:
+    def __mul__(self, other):
+        if not isinstance(other, RuntimedFunc):
+            raise TypeError(f"'{other}' is not a valid runtimed function.")
+
+        comp_func = runtime_comp(self, other)
+        return RuntimedFunc(comp_func)
+
+
+# -------------------------
+#     Hinted Functions
+# -------------------------
+class HintedDomFunc(PlainFunc):
     """
     The class of 'domain-hinted functions':
         1. defined domain (based on type hints)
-        2. defined composition
-    It is a subclass of 'Func'
+        2. defined comp
+    It is a subclass of 'PlainFunc'
     """
-    pass
+    def __init__(self, func):
+        super().__init__(func)
+        self._hinted_domain = hinted_domain(func)
 
-# TODO
-class CodHintedFunc:
+    @property
+    def domain(self):
+        return self._hinted_domain
+
+
+class HintedCodFunc(PlainFunc):
     """
     The class of 'codomain-hinted functions':
         1. defined codomain (based on type hints)
-        2. defined composition
-    It is a subclass of 'Func'
+        2. defined comp
+    It is a subclass of 'PlainFunc'
     """
-    pass
+    def __init__(self, func):
+        super().__init__(func)
+        self._hinted_codomain = hinted_codomain(func)
 
-# TODO
-class HintedFunc:
+    @property
+    def codomain(self):
+        return self._hinted_codomain
+
+
+class HintedFunc(HintedDomFunc, HintedCodFunc):
     """
     The class of 'hinted functions':
         1. have type hints
         2. defined domain and codomain (based on type hints)
-        3. safe composition (based on type hints)
+        3. safe comp (based on type hints)
     It is a subclass of:
-        1. 'DomHintedFunc'
-        2. 'CodHintedFunc'
+        1. 'HintedDomFunc'
+        2. 'HintedCodFunc'
     """
-    pass
+    def __init__(self, func):
+        HintedDomFunc.__init__(self, func)
+        HintedCodFunc.__init__(self, func)
 
-# TODO
-class DomTypedFunc:
+    def __mul__(self, other):
+        if not isinstance(other, HintedFunc):
+            raise TypeError(f"'{other}' is not a valid hinted function.")
+
+        comp_func = hinted_comp(self, other)
+        return HintedFunc(comp_func)
+
+
+# ---------------------------
+#       Typed Functions
+# ---------------------------
+class TypedDomFunc(HintedDomFunc, RuntimedDomFunc):
     """
     The class of 'domain-typed functions':
         1. defined and checked domain
-        2. defined composition
+        2. defined comp
     It is a subclass of:
-        1. 'DomHintedFunc'
-        2. 'DomRuntimedFunc'
+        1. 'HintedDomFunc'
+        2. 'RuntimedDomFunc'
     """
-    pass
+    def __init__(self, func):
+        HintedDomFunc.__init__(self, func)
+        RuntimedDomFunc.__init__(self, func)
+        check_domain(func)
 
-# TODO
-class CodTypedFunc:
+    @property
+    def domain(self):
+        return self._hinted_domain
+
+    def __call__(self, *args, **kwargs):
+        check_domain(self.func)
+        return super().__call__(*args, **kwargs)
+
+
+class TypedCodFunc(HintedCodFunc, RuntimedCodFunc):
     """
-    The class of 'codomain-hinted functions':
+    The class of 'codomain-typed functions':
         1. defined and checked codomain
-        2. defined composition
+        2. defined comp
     It is a subclass of:
         1. 'CodHintedFunc'
         2. 'CodRuntimedFunc'
     """
-    pass
+    def __init__(self, func):
+        HintedCodFunc.__init__(self, func)
+        RuntimedCodFunc.__init__(self, func)
+        check_codomain(func)
 
-# TODO: refactor according to the above descriptions
-class TypedFunc:
+    @property
+    def codomain(self):
+        return self._hinted_codomain
+
+    def __call__(self, *args, **kwargs):
+        result = super().__call__(*args, **kwargs)
+        check_codomain(self.func)
+        return result
+
+
+class TypedFunc(TypedDomFunc, TypedCodFunc, RuntimedFunc, HintedFunc):
     """
     The class of 'typed functions':
         1. have type hints
         2. type hints are checked at runtime
         3. defined domain and codomain (based on type hints)
-        4. safe composition (based on type hints)
+        4. safe comp (based on type hints)
     It is a subclass of:
-        1. 'DomTypedFunc'
-        2. 'CodTypedFunc'
+        1. 'TypedDomFunc'
+        2. 'TypedCodFunc'
         3. 'RuntimedFunc'
         4. 'HintedFunc'
     """
     def __init__(self, func):
         if not callable(func):
             raise TypeError(f"'{func}' is not a function.")
-        self.func = func
-        self._type_hints = get_type_hints(func)
-        if not self._type_hints:
-            raise TypeError(f"The function '{func.__name__}' lacks type hints.")
-        self._domain = self.get_domain()
-        self._codomain = self.get_codomain()
-
-    def get_domain(self):
-        type_hints = get_type_hints(self.func)
-        domain_hints = tuple(type_hints.values())[:-1]
-        if domain_hints:
-            return prod_type_(*domain_hints)
-        return type(None)
-
-    def get_codomain(self):
-        type_hints = get_type_hints(self.func)
-        return_hint = list(type_hints.values())[-1]
-        if return_hint:
-            return return_hint
-        return type(None)
-
-    @property
-    def domain(self):
-        return self._domain
-
-    @property
-    def codomain(self):
-        return self._codomain
+        super().__init__(func)
 
     def __call__(self, *args, **kwargs):
-        bound_args = inspect.signature(self.func).bind(*args, **kwargs)
-        bound_args.apply_defaults()
-
-        for arg_name, arg_value in bound_args.arguments.items():
-            if arg_name in self._type_hints:
-                expected_type = self._type_hints[arg_name]
-                if not isinstance(arg_value, expected_type):
-                    raise TypeError(f"Argument '{arg_name}' is not of type '{expected_type.__name__}'.")
+        check_domain(self.func)
         try:
             result = self.func(*args, **kwargs)
         except Exception as e:
             raise TypeError(f"Function '{self.func.__name__}' raised an error: {e}")
-
-        if 'return' in self._type_hints:
-            expected_return_type = self._type_hints['return']
-            if not isinstance(result, expected_return_type):
-                raise TypeError(f"Return '{result}' is not of type '{expected_return_type.__name__}'.")
+        check_codomain(self.func)
 
         return result
 
@@ -173,10 +231,5 @@ class TypedFunc:
         if not isinstance(other, TypedFunc):
             raise TypeError(f"'{other}' is not a valid typed function.")
 
-        if not issubclass(other.codomain, self.domain):
-            raise TypeError(f"Codomain '{other.codomain.__name__}' of '{other.func.__name__}' is not a subtype of the domain '{self.domain.__name__}' of '{self.func.__name__}'.")
-
-        def comp(*args, **kwargs):
-            return self.func(other.func(*args, **kwargs))
-
-        return TypedFunc(comp)
+        comp_func = typed_comp(self, other)
+        return TypedFunc(comp_func)
