b3cfc92e8e6df94b1b1f8fe57fe42526090c88df
yxm <yxm.dev@gmx.ie>
2025-02-13 11:04:08 -0300

    feat: improve inter dspec

--- f_core/mods/spec/helper_.py
+++ f_core/mods/spec/helper_.py
@@ -8,8 +8,21 @@ def curry_(f, x):
         raise TypeError(f'{x} is not a tuple.')
     return lambda *args: f(*x, *args)
 
-def inter_seq(*seqs):
-    return [item for item in seqs[0] if all(seq[i] == item for seq in seqs for i in range(len(seq)) if i < len(seqs[0]))]
+def inter_str(*strings):
+    return ''.join(char for i, char in enumerate(strings[0]) if all(i < len(string) and char == string[i] for string in strings[1:]))
 
-def inter_cont(*conts):
+def inter_tuple(*seqs):
+    if not seqs:
+        return tuple()
+    return tuple(item for item in seqs[0] if all(item in seq for seq in seqs))
+
+def inter_list(*seqs):
+    if not seqs:
+        return []
+    return [item for item in seqs[0] if all(item in seq for seq in seqs)]
+
+def inter_set(*conts):
     return set(conts[0]).intersection(*conts[1:])
+
+def inter_dict(*dicts):
+    return { key: dicts[0][key] for key in dicts[0].keys() if all(key in d and dicts[0][key] == d[key] for d in dicts[1:])}
